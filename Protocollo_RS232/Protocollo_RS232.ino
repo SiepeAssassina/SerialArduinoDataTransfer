e1d59acb72b342c3ef9ceff034e6edb6f0cdc36a
SiepeAssassina <lorenzo.tomasin@live.it>
2014-02-15 17:36:16 +0100

    Alpha_0.0.1

---
+++ Protocollo_RS232/Protocollo_RS232.ino
@@ -0,0 +1,65 @@
+#include <LiquidCrystal.h>
+#define IDLE 0x0
+#define READING_DATA 0x1
+#define DATA_END 0x2
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup()
+{
+  lcd.begin(16, 2);
+  Serial.begin(1200);
+}
+byte ca[8] = {
+  '=', '=', '=', '=', '=', '=', '=', '='};
+int state = 0x0;
+void loop() 
+{
+  Serial.flush();
+  if(Serial.read() == 'R')  //RQT
+  {    
+    lcd.clear();
+    lcd.write("RQT");
+    lcd.clear();
+    Serial.write('A');      //ACK
+    lcd.write("WAITING..."); 
+    while(Serial.available() == 0) ;      
+    while(state != DATA_END) 
+    {       
+      switch (Serial.read())
+      {
+      case 'D':
+        if(state == IDLE)
+         for(int i = 0; i < 8; i++)
+         ca[i] = ' ';
+        state = READING_DATA; 
+        Serial.write('A');
+        lcd.setCursor(0, 0);
+        lcd.clear();
+        lcd.write("DATA");          
+        while(Serial.available() < 2) ;
+        ca[Serial.read()] = Serial.read();    
+        delay(100);        
+        break;
+      case 'C':
+        lcd.clear();
+        break;
+      case 'S':
+        state = DATA_END;
+        break;
+      } 
+    }
+    lcd.clear();
+    lcd.write("DATA_END");
+    state = IDLE;
+    lcd.clear();
+    lcd.write("WRITING");
+    for(int i = 0; i < 8; i++)
+    {
+      lcd.setCursor(i, 1);
+      lcd.write(ca[i]);
+    }
+  }
+}
+
+
+
+
---
+++ SerialConsole/SerialConsole/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+
+namespace SerialConsole
+{    
+    class Program
+    {        
+        static public SerialPort com;
+        static void Main(string[] args)
+        {
+            char[] buffer = new char[1];
+            com = new SerialPort("COM3");
+            com.BaudRate = 1200;
+            com.Parity = Parity.None;
+            com.StopBits = StopBits.One;
+            com.Handshake = Handshake.None;
+           // com.ReadTimeout = 1000;
+            com.ReadBufferSize = 16;
+            com.Open();
+           /* while (true)
+            {
+                com.Write((string)Console.ReadLine());
+                try
+                {
+                    Console.WriteLine((char)com.ReadByte());
+                }
+                catch (TimeoutException) {
+                    Console.WriteLine("NULL");
+                }
+            }*/            
+            while (true)
+            {
+                com.Write("R");
+                waitForCom('A');
+                string S = Console.ReadLine();
+                if (S.Length < 9)
+                    sendString(S);
+                Console.Read();
+            }
+        }
+
+        static void waitForCom(char c)
+        {
+            while(com.ReadByte() != c) ;
+        }
+
+        static void sendString(string s)
+        {           
+            for (int i = 0; i < s.Length; i++)
+            {  
+                com.Write("D" + (char)i + s[i]);
+                waitForCom('A');
+            }
+            com.Write("S");
+        }
+    }
+}
